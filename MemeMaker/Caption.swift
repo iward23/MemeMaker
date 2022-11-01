f65fc31dcff3bf13d64139ce98f78f37d705577e
Imani Ward <idward712@gmail.com>
2022-11-01 14:23:44 -0400

    o

---
+++ MemeMaker/Caption.swift
@@ -0,0 +1,12 @@
+//
+//  Caption.swift
+//  MemeMaker
+//
+//  Created by 9i on 11/1/22.
+//
+
+import Foundation
+struct Caption{
+    var emoji: String
+    var caption: String
+}
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var bottomCaption: UILabel!
+    @IBOutlet var topCaption: UILabel!
+    
+    @IBOutlet var topSegementicControl: UISegmentedControl!
+    @IBOutlet var bottomSegmenticControl: UISegmentedControl!
+    
+    
+    @IBAction func didValueChange(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
