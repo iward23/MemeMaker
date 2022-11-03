3739e75c3742f6974b8037a98e17d1af0bd7d216
Imani Ward <idward712@gmail.com>
2022-11-03 14:19:39 -0400

    o

--- MemeMaker/Caption.swift
+++ MemeMaker/Caption.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-struct captionOption{
+struct captionOption {
     var emoji: String
     var caption: String
 }
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -10,19 +10,35 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    
     @IBOutlet var topSegmentedControl: UISegmentedControl!
     @IBOutlet var bottomSegmentedControl: UISegmentedControl!
     @IBOutlet var topCaptionLabel: UILabel!
     @IBOutlet var bottomCaptionLabel: UILabel!
+    
+    var topOptions = [captionOption(emoji:"🤠", caption: "Hey there, cowboy!"), captionOption(emoji: "🫰", caption: "Saranghae")]
+    
+    var bottomOptions = [captionOption(emoji:"🙈", caption: "I can't see"),captionOption(emoji: "🙉", caption: "I can't hear")]
     
     @IBAction func segmentedControls(_ sender: UISegmentedControl) {
         
     }
     
     
+  func 
+
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        topSegmentedControl.removeAllSegments()
+        topSegmentedControl.selectedSegmentIndex = 0
+        for choice in topOptions{
+            topSegmentedControl.insertSegment(withTitle: choice.emoji, at: topOptions.count, animated: false)
+        }
     }
 
 
